2aacb549741ef08455c28ae4756d3e4c1ec962fd
Markus Pullmann <markus.pullmann@gmx.de>
2017-01-08 09:50:03 +0100

    First version, no localization

---
+++ chrome/content/overlay.js
@@ -0,0 +1,49 @@
+var alreadyOpen = false;
+var state = "";
+
+window.addEventListener("load", function(e) { 
+	startup(); 
+}, false);
+
+
+function startup() {
+	setInterval(function() {
+		var invitationOpen = document.getElementById('calendar-invitations-panel') != null && !document.getElementById('calendar-invitations-panel').hidden && document.getElementById('calendar-invitations-label') != null && document.getElementById('calendar-invitations-label').value.length > 0;
+		
+		// first invitation!
+		if (invitationOpen && !alreadyOpen) {
+			document.getElementById('calendar-invitations-panel').click();
+			alreadyOpen = true;
+			state = document.getElementById('calendar-invitations-label').value;
+			popup("Ausstehende Termineinladung(en)", state);
+			document.getElementById('lin').label = 'LIN 1';
+			return;
+		}
+		
+		// no more invitations
+		if (!invitationOpen) {
+			alreadyOpen = false;
+			document.getElementById('lin').label = 'LIN 0';
+			state = document.getElementById('calendar-invitations-label').value;
+			return;
+		}
+		
+		// new additional invitation
+		if (state != document.getElementById('calendar-invitations-label').value) {
+			state = document.getElementById('calendar-invitations-label').value;
+			popup("Ausstehende Termineinladung(en)", state);
+			document.getElementById('lin').label = 'LIN 2';
+			return;
+		}
+	}, 5000);
+}
+
+function popup(title, text) {
+  try {
+    Components.classes['@mozilla.org/alerts-service;1']
+              .getService(Components.interfaces.nsIAlertsService)
+              .showAlertNotification(null, title, text, false, '', null);
+  } catch(e) {
+    // prevents runtime error on platforms that don't implement nsIAlertsService
+  }
+}
