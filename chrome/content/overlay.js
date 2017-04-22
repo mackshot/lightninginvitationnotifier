1fe478e699bda9b1041cb133b72eb1315abb55c0
Markus Pullmann <markus.pullmann@gmx.de>
2017-04-22 08:17:09 +0200

    Support for Thunderbird 52

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,53 +1,58 @@
 const Cu = Components.utils;
 Cu.import("resource:///modules/StringBundle.js"); // for StringBundle
 
-var alreadyOpen = false;
+var dialogOpen = false;
 var state = "";
 
 window.addEventListener("load", function(e) { 
-	startup(); 
+    startup();
 }, false);
 
 let strings = new StringBundle("chrome://lightninginvitationnotifier/locale/message.properties");
 
 function startup() {
 	setInterval(function() {
-		var invitationOpen = document.getElementById('calendar-invitations-panel') != null && !document.getElementById('calendar-invitations-panel').hidden && document.getElementById('calendar-invitations-label') != null && document.getElementById('calendar-invitations-label').value.length > 0;
-		
+		var invitationPending = document.getElementById('calendar-invitations-panel') != null && !document.getElementById('calendar-invitations-panel').hidden && document.getElementById('calendar-invitations-label') != null && document.getElementById('calendar-invitations-label').value.length > 0;
+
+		var update = function () {
+            state = document.getElementById('calendar-invitations-label').value;
+            if (invitationPending) {
+                popup(strings.get("invitations"), state);
+                document.getElementById('lin').label = 'LIN: ' + state;
+            } else {
+                document.getElementById('lin').label = 'LIN';
+			}
+		};
+
 		// first invitation!
-		if (invitationOpen && !alreadyOpen) {
+		if (invitationPending && !dialogOpen) {
 			document.getElementById('calendar-invitations-panel').click();
-			alreadyOpen = true;
-			state = document.getElementById('calendar-invitations-label').value;
-			popup(strings.get("invitations"), state);
-			document.getElementById('lin').label = 'LIN 1';
+			dialogOpen = true;
+            update();
 			return;
 		}
 		
 		// no more invitations
-		if (!invitationOpen) {
-			alreadyOpen = false;
-			document.getElementById('lin').label = 'LIN 0';
-			state = document.getElementById('calendar-invitations-label').value;
+		if (!invitationPending) {
+			dialogOpen = false;
+			update();
 			return;
 		}
 		
 		// new additional invitation
 		if (state != document.getElementById('calendar-invitations-label').value) {
-			state = document.getElementById('calendar-invitations-label').value;
-			popup(strings.get("invitations"), state);
-			document.getElementById('lin').label = 'LIN 2';
+            update();
 			return;
 		}
 	}, 5000);
 }
 
 function popup(title, text) {
-  try {
-    Components.classes['@mozilla.org/alerts-service;1']
-              .getService(Components.interfaces.nsIAlertsService)
-              .showAlertNotification(null, title, text, false, '', null);
-  } catch(e) {
-    // prevents runtime error on platforms that don't implement nsIAlertsService
-  }
+    try {
+        Components.classes['@mozilla.org/alerts-service;1']
+                  .getService(Components.interfaces.nsIAlertsService)
+                  .showAlertNotification(null, title, text, false, '', null);
+    } catch(e) {
+        // prevents runtime error on platforms that don't implement nsIAlertsService
+    }
 }
