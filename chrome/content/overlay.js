983d8b7dfc4bd23154ec7753e257c50bfcdc41ac
Markus Pullmann <markus.pullmann@gmx.de>
2017-06-30 10:16:27 +0200

    Incompatibility with "Search as list" resolved!

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,53 +1,53 @@
 const Cu = Components.utils;
 Cu.import("resource:///modules/StringBundle.js"); // for StringBundle
 
-var dialogOpen = false;
-var state = "";
+var LIN_dialogOpen = false;
+var LIN_state = "";
 
 window.addEventListener("load", function(e) { 
-    startup();
+    LIN_startup();
 }, false);
 
 let strings = new StringBundle("chrome://lightninginvitationnotifier/locale/message.properties");
 
-function startup() {
+function LIN_startup() {
 	setInterval(function() {
 		var invitationPending = document.getElementById('calendar-invitations-panel') != null && !document.getElementById('calendar-invitations-panel').hidden && document.getElementById('calendar-invitations-label') != null && document.getElementById('calendar-invitations-label').value.length > 0;
 
 		var update = function () {
-            state = document.getElementById('calendar-invitations-label').value;
+            LIN_state = document.getElementById('calendar-invitations-label').value;
             if (invitationPending) {
-                popup(strings.get("invitations"), state);
-                document.getElementById('lin').label = 'LIN: ' + state;
+                LIN_popup(strings.get("invitations"), LIN_state);
+                document.getElementById('lin').label = 'LIN: ' + LIN_state;
             } else {
                 document.getElementById('lin').label = 'LIN';
 			}
 		};
 
 		// first invitation!
-		if (invitationPending && !dialogOpen) {
+		if (invitationPending && !LIN_dialogOpen) {
 			document.getElementById('calendar-invitations-panel').click();
-			dialogOpen = true;
+			LIN_dialogOpen = true;
             update();
 			return;
 		}
 		
 		// no more invitations
 		if (!invitationPending) {
-			dialogOpen = false;
+			LIN_dialogOpen = false;
 			update();
 			return;
 		}
 		
 		// new additional invitation
-		if (state != document.getElementById('calendar-invitations-label').value) {
+		if (LIN_state != document.getElementById('calendar-invitations-label').value) {
             update();
 			return;
 		}
 	}, 5000);
 }
 
-function popup(title, text) {
+function LIN_popup(title, text) {
     try {
         Components.classes['@mozilla.org/alerts-service;1']
                   .getService(Components.interfaces.nsIAlertsService)
