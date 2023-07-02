cb59fc8ec5a36de0604604fa95e811fbfbf6f700
Markus Pullmann <markus@pullmann.one>
2023-07-02 11:45:11 +0200

    Compatibility	with Thunderbird 115+

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm lightninginvitationnotifier.xpi
-7z a -tzip -x!.git -x!.idea -x!.gitignore -x!deploy.sh lightninginvitationnotifier.xpi *
+7zz a -tzip -x!.git -x!.idea -x!.gitignore -x!deploy.sh -x!deploy.bat lightninginvitationnotifier.xpi *
